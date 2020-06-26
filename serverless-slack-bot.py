fbc124a4a7fad0ecab6513360f1e8e2b43b8966b
Aswin Kumar <aswin.kumar@autodesk.com>
2020-06-26 20:19:45 +0530

    SSB: Fix urllib request library import issue

--- serverless-slack-bot.py
+++ serverless-slack-bot.py
@@ -4,6 +4,7 @@ Serverles Slack Bot Lambda handler.
 import os
 import json
 import urllib
+import urllib.request
 from utils.log_util import logger
 
 # Get Bot User OAuth Access token from environment
@@ -30,58 +31,60 @@ def handler(event, context):
                     'body': json.dumps(challenge_response)
                 }
 
-        slack_event = request_body_json['event']
-        if 'bot_id' in slack_event:
-            # Capture Bot event if needed
-            # Ignore Bot event if not needed
-            logger.warn('Ignored bot event')
-        else:
-            # Capture User event
-            # Get the text of the user message sent to the bot
-            user_message = slack_event['text']
-            logger.info('User Message: {}'.format(user_message))
+            if 'event' in request_body_json:
+                slack_event = request_body_json['event']
+                logger.info('Received Slack Event with Body: {}'.format(slack_event))
+                if 'bot_id' in slack_event:
+                    # Capture Bot event if needed
+                    # Ignore Bot event if not needed
+                    logger.warn('Ignored bot event')
+                else:
+                    # Capture User event
+                    # Get the text of the user message sent to the bot
+                    user_message = slack_event['text']
+                    logger.info('User Message: {}'.format(user_message))
 
-            # Create your Bot Reply logic here
-            # For now - this is a hardcoded reply
-            """In ideal cases - configure a NLP service like Watson Assistant or Rasa NLU
-            to respond to a natural languages user text"""
-            bot_reply = "Hello I am the Serverless Slack Bot"
+                    # Create your Bot Reply logic here
+                    # For now - this is a hardcoded reply
+                    """In ideal cases - configure a NLP service like Watson Assistant or Rasa NLU
+                    to respond to a natural languages user text"""
+                    bot_reply = "Hello I am the Serverless Slack Bot"
 
-            # Get the ID of the channel where the message was posted.
-            channel_id = slack_event["channel"]
+                    # Get the ID of the channel where the message was posted.
+                    channel_id = slack_event["channel"]
 
-            if len(user_message) > 0:
-                # Create an associative array and URL-encode it
-                # The Slack API doesn't not handle JSON
-                data = urllib.parse.urlencode(
-                    (
-                        ("token", BOT_TOKEN),
-                        ("channel", channel_id),
-                        ("text", bot_reply)
-                    )
-                )
-                data = data.encode("ascii")
+                    if len(user_message) > 0:
+                        # Create an associative array and URL-encode it
+                        # The Slack API doesn't not handle JSON
+                        data = urllib.parse.urlencode(
+                            (
+                                ("token", BOT_TOKEN),
+                                ("channel", channel_id),
+                                ("text", bot_reply)
+                            )
+                        )
+                        data = data.encode("ascii")
 
-                # Construct the HTTP request that will be sent to the Slack API.
-                request = urllib.request.Request(
-                    SLACK_URL,
-                    data=data,
-                    method="POST"
-                )
+                        # Construct the HTTP request that will be sent to the Slack API.
+                        request = urllib.request.Request(
+                            SLACK_URL,
+                            data=data,
+                            method="POST"
+                        )
 
-                # Add a header mentioning that the text is URL-encoded.
-                request.add_header(
-                    "Content-Type",
-                    "application/x-www-form-urlencoded"
-                )
+                        # Add a header mentioning that the text is URL-encoded.
+                        request.add_header(
+                            "Content-Type",
+                            "application/x-www-form-urlencoded"
+                        )
 
-                # Fire the request
-                urllib.request.urlopen(request).read()
+                        # Fire the request
+                        urllib.request.urlopen(request).read()
 
-                # Success
-                response = {
-                    'status_code': 200,
-                }
+                        # Success
+                        response = {
+                            'status_code': 200,
+                        }
         logger.info("Response: {}".format(response))
         return response
     except Exception as e:
